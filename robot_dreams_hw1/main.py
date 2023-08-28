3eed6e657dc3a54d9d4e4d0a12db7d69ff6b4182
Illia <gorbasenko245@gmail.com>
2023-08-28 15:50:30 +0300

    add 1 hw;

---
+++ robot_dreams_hw1/main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# sum
+number1 = input('Enter first number: ')
+number2 = input('Enter first number2: ')
+
+print('Sum: ', float(number1) + float(number2))
+
+# temperature converting
+
+temperature_value = float(input('Enter temperature value: '))
+temperature_type = input('Enter temperature type(C/F): ')
+
+match temperature_type.upper():
+    case 'F':
+        print('Your temperature was converted to Celsius. Value: ', (temperature_value - 32)/1.8)
+    case 'C':
+        print('Your temperature was converted to Fahrenheit. Value: ', temperature_value * 1.8 + 32)
+    case _:
+        print("Unsupported temperature type")
